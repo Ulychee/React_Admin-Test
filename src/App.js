df2395513fc77f3f46f81d4a24c916949fbbbc11
lychee <1051769585@qq.com>
2020-12-14 18:56:52 +0800

    add new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 // import { Admin } from 'react-admin'
-import { Admin, Resource, ListGuesser } from 'react-admin'
+import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin'
 import jsonServerProvider from 'ra-data-json-server'
 import { UserList } from './myStyle/users'
 import { PostList } from './myStyle/PostList'
@@ -12,7 +12,8 @@ const App = () => {
   return (
     <Admin dataProvider={dataProvider}>
       {/* <Resource name="posts" list={ListGuesser} />   */}
-      <Resource name="posts" list={PostList} />  
+      {/* <Resource name="posts" list={PostList} />   */}
+      <Resource name="posts" list={PostList} edit={EditGuesser} />
       <Resource name="users" list={UserList} />
     </Admin>
   )
--- src/myStyle/PostList.js
+++ src/myStyle/PostList.js
@@ -1,13 +1,44 @@
-import * as React from 'react'
-import { List, Datagrid, TextField, ReferenceField } from 'react-admin';
+import * as React from "react";
+import {
+    List,
+    Datagrid,
+    TextField,
+    ReferenceField,
+    EditButton,
+    Edit,
+    SimpleForm,
+    ReferenceInput,
+    SelectIpnut,
+    TextInput,
+} from 'react-admin';
 
 export const PostList = props => (
   <List {...props}>
       <Datagrid rowClick="edit">
-          <ReferenceField source="userId" reference="users"><TextField source="id" /></ReferenceField>
           <TextField source="id" />
+          <ReferenceField source="userId" reference="users">
+            {/* <TextField source="id" /> */}
+            <TextField source="name" />
+          </ReferenceField>
+          {/* <TextField source="id" /> */}
           <TextField source="title" />
-          <TextField source="body" />
+          {/* <TextField source="body" /> */}
+          <EditButton />
       </Datagrid>
   </List>
+);
+
+export const PostEdit = props => (
+  <Edit {...props}>
+      <SimpleForm>
+        <TextInput disabled source="id" />
+          <ReferenceInput source="userId" reference="users">
+              {/* <SelectInput optionText="id" /> */}
+          </ReferenceInput>
+          {/* <TextInput source="id" /> */}
+          <TextInput source="title" />
+          {/* <TextInput source="body" /> */}
+          <TextInput multiline source="body" />
+      </SimpleForm>
+  </Edit>
 );
